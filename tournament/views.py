1250f7bd351035c59e26488af0d4d1c3d1055c3a
Pasha <pavik.varganov@gmail.com>
2024-09-21 16:14:01 +0300

    create home page

--- tournament/views.py
+++ tournament/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from tournament.models import Player, Team, GameSession, League
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_players = Player.objects.count()
+    num_teams = Team.objects.count()
+    num_game_sessions = GameSession.objects.count()
+    num_leagues = League.objects.count()
+
+    context = {
+        "num_players": num_players,
+        "num_teams": num_teams,
+        "num_game_sessions": num_game_sessions,
+        "num_leagues": num_leagues,
+    }
+
+    return render(request, "tournament/index.html", context=context)
